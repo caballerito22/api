c2a69ca8db20e9e9e563e4fc1878ec9c26d5e00e
caballerito22 <alecabnun@alu.edu.gva.es>
2025-06-02 21:43:39 +0200

    revisado y comentado

--- src/main/java/com/ciudaddeportiva/api/model/UsuarioResponse.java
+++
@@ -1,9 +0,0 @@
-package com.ciudaddeportiva.api.model;
-
-//maneja los datos públicos de un usuario (sin contreseña-9
-public class UsuarioResponse {
-    private long id;
-    private String email;
-    public long getId() { return id; }
-    public String getEmail() { return email; }
-}
